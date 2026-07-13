71f61a3efccd0d4077646ec57862cf75531a9d3a
K <kadeemmighty@gmail.com>
2026-07-13 16:04:32 -0400

    Minor bug fix

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -9,15 +9,13 @@ const FetchData = () => {
     <>
      <ul className='list_data_main'>
         <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
-        {data && data.map((e) => (
-            <>
+        {data && data.map((e, index) => (
             <li key={index} className='list_data'>
                 <h3>{e.name}</h3>
                 <p><strong>Importance: </strong>{e.importance}</p>
                 <p><strong>Benefits: </strong>{e.benefits}</p>
                 <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
             </li>
-            </>
         ))}
      </ul>
     </>
